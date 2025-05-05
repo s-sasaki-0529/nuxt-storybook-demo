7a64a3fcc7fc67b571ac1f00a31ba47080f22685
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2025-05-05 17:34:10 +0900

    パッケージのバージョンを調整

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   typescript: {
     strict: true,
   },
+
   ssr: false,
+
   app: {
     baseURL: "/",
     buildAssetsDir: "/_nuxt/",
@@ -12,7 +15,10 @@ export default defineNuxtConfig({
       title: "Nuxt Storybook Demo",
     },
   },
+
   imports: {
     autoImport: true,
   },
+
+  compatibilityDate: "2025-05-05",
 });
